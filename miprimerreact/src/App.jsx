5276831beff7fe7a5dccb5005d2a6a98c05c2f53
Abner Jimenez <71862339+abnerjg@users.noreply.github.com>
2023-11-29 15:41:19 -0600

    first commit

---
+++ miprimerreact/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='cuadro'>
+      <p className='mensaje'>Hola Abner</p>
+      <button className='boton'>ORDER NOW</button>
+    </div> 
+  )
+}
+
+export default App
